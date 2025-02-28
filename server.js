6eb589ae74f9866d8220e03b58c885f9d13d49ba
Goblinelettronica <123337392+Goblinelettronica@users.noreply.github.com>
2025-02-28 15:53:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 5001;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'caricamento.html'));
 });
 const url = `http://localhost:${PORT}`;
 
